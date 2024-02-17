e3020dfbf0f61dcdb85544b0270efd7193df1ac8
nuriddin-coder <sirojiddinovnuriddin717@gmail.com>
2024-02-17 17:19:40 +0500

    new change

--- src/components/Drawer/drawer.jsx
+++ src/components/Drawer/drawer.jsx
@@ -12,10 +12,9 @@ export const Drawer = () => {
     <div className="drawer drawer-end">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <label
-          htmlFor="my-drawer-4"
-          className="border-[2px] border-primary p-[10px]"
-        >
+        {/* Page content here */}
+
+        <label htmlFor="my-drawer-4" className="drawer-button">
           <MenuIcon />
         </label>
       </div>
@@ -25,7 +24,7 @@ export const Drawer = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
           {Links.map((link) => (
             <>
               <li key={link.path}>
@@ -38,3 +37,25 @@ export const Drawer = () => {
     </div>
   );
 };
+
+{
+  /*
+   <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {Links.map((link) => (
+            <>
+              <li key={link.path}>
+                <NavLink to={link.path ? link.path : "/"}>{link.name}</NavLink>
+              </li>
+            </>
+          ))}
+        </ul>
+
+
+         <label
+          htmlFor="my-drawer-4"
+          className="border-[2px] border-primary p-[10px]"
+        >
+          <MenuIcon />
+        </label>
+*/
+}
