0054d6344e8bf64caca82d902f36f5d43491d9b4
nuriddin-coder <sirojiddinovnuriddin717@gmail.com>
2024-02-18 19:15:43 +0500

    new change

--- src/components/Btn/buttons.jsx
+++ src/components/Btn/buttons.jsx
@@ -4,16 +4,19 @@ import clsx from "clsx";
 export const Buttons = ({ variant, icon: Icon, children }) => {
   return (
     <button
-      className={clsx("flex items-center justify-center px-[30px] py-[14px]", {
-        "gap-[10px] rounded-[14px] bg-primary text-[20px] font-bold text-white":
-          variant === "primary",
-        " gap-[10px] rounded-[14px] bg-funkyYellow text-[20px] font-bold  text-primary":
-          variant === "secondary",
-        " gap-[10px] rounded-[14px] border-[1px] border-primary bg-white text-[20px] font-bold  text-primary":
-          variant === "border",
-        " gap-[10px]  rounded-[14px] bg-white text-[20px] font-bold  text-primary":
-          variant === "default",
-      })}
+      className={clsx(
+        "flex items-center justify-center whitespace-nowrap px-[30px] py-[14px]",
+        {
+          "gap-[10px] rounded-[14px] bg-primary text-[20px] font-bold text-white":
+            variant === "primary",
+          " gap-[10px] rounded-[14px] bg-funkyYellow text-[20px] font-bold  text-primary":
+            variant === "secondary",
+          " gap-[10px] rounded-[14px] border-[1px] border-primary bg-white text-[20px] font-bold  text-primary":
+            variant === "border",
+          " gap-[10px]  rounded-[14px] bg-white text-[20px] font-bold  text-primary":
+            variant === "default",
+        },
+      )}
     >
       {Icon ? <Icon /> : ""}
       {children}
